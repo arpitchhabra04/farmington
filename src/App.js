5a32a47646c43a3ee2d58665172f609401513942
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-11-03 19:49:10 +0530

    testimonials created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/headerComponent/headerComponent';
+import Testimonials from './components/testimonialComponent/testimonials';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      {/* <Header></Header> */}
+      <Testimonials></Testimonials>
     </div>
   );
 }
