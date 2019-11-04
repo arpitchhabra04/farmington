0502b77c65660c8dd0a108b869593d43a1675a24
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-11-04 18:31:44 +0530

    about us css

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   return (
     <div className="App">
       {/* <Header></Header> */}
-      {/* <Testimonials></Testimonials> */}
+
       <AboutUs></AboutUs>
+      <Testimonials></Testimonials>
     </div>
   );
 }
--- src/components/aboutComponent/aboutUs.js
+++ src/components/aboutComponent/aboutUs.js
@@ -6,19 +6,19 @@ class AboutUs extends Component {
   state = {};
   render() {
     return (
-      <div>
+      <div className="about_main">
         <Row>
-          <Col offset={1} md={11}>
-            <h1>About Farmington</h1>
-            <h2>With Us the Grass is Greener</h2>
-            <p>
+          <Col offset={1} md={11} className="about_1">
+            <p className="head1">About Farmington</p>
+            <p className="subhead1">With Us the Grass is Greener</p>
+            <p className="description1">
               I'm a paragraph. Click here to add your own text and edit me. It’s
               easy. Just click “Edit Text” or double click me to add your own
               content and make changes to the font. Feel free to drag and drop
               me anywhere you like on your page. I’m a great place for you to
               tell a story and let your users know a little more about you.
             </p>
-            <p>
+            <p className="description1">
               This is a great space to write long text about your company and
               your services. You can use this space to go into a little more
               detail about your company. Talk about your team and what services
@@ -29,23 +29,33 @@ class AboutUs extends Component {
             </p>
           </Col>
           <Col md={11}>
-            <div className="image_farmer">
-              <img src="images/farm.jfif" alt="farmer image" />
+            <div>
+              <img
+                className="image_farmer"
+                src="images/farm3.jfif"
+                alt="farmer image"
+              />
             </div>
           </Col>
         </Row>
         <Row>
           <Col offset={1} md={11}>
             <div>
-              <img src="images/farm2.jfif" alt="farmer image"></img>
+              <img
+                src="images/farm5.jfif"
+                alt="farmer image"
+                className="image_farmer"
+              ></img>
             </div>
           </Col>
-          <Col md={11} className="sub_about">
-            <h1>Get Free Help From Experts</h1>
+          <Col md={11} className="about_2">
+            <h1 className="head2">Get Free Help From Experts</h1>
 
-            <Button className="about_button">LogIn</Button>
+            <Button size="large" className="subhead2">
+              LogIn
+            </Button>
 
-            <h2> Or Call Us: 123-456-7890 </h2>
+            <h2 className="description2"> Or Call Us: 123-456-7890 </h2>
           </Col>
         </Row>
       </div>
