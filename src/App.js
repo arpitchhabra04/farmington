54a97c06742770e9277621bb6026227c6e39cc56
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-11-06 17:10:18 +0530

    service component completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Testimonials from './components/testimonialComponent/testimonials';
 import AboutUs from './components/aboutComponent/aboutUs';
 import Contact from './components/contactComponent/contactUs';
 import Relaible from './components/reliableComponent/reliable';
+import Services from './components/servicesComponent/services';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      {/* asjdha */}
+      <Services></Services>
       <AboutUs></AboutUs>
       <Testimonials></Testimonials>
       <Relaible></Relaible>
